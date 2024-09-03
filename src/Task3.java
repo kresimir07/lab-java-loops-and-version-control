a155793e0b9d1d0f0a49d7400f4c2800739156d5
kresimir <kresimir.vulic07@gmail.com>
2024-09-03 19:04:50 +0200

    done

--- src/Task3.java
+++ src/Task3.java
@@ -22,24 +22,5 @@ public class Task3 {
 
     }
 
-
-
-
-//public class MathExpression {
-//
-//    public static double calculateExpression(double x, double y) {
-//        double firstTerm = Math.pow(x, 2); // x^2
-//        double secondTerm = Math.pow((4 * y / 5) - x, 2); // (4y/5 - x)^2
-//        return firstTerm + secondTerm;
-//    }
-//
-//    public static void main(String[] args) {
-//        double x = 3.0; // example value for x
-//        double y = 4.0; // example value for y
-//        double result = calculateExpression(x, y);
-//        System.out.println("The result of the expression is: " + result);
-//    }
-//}
-
     }
 
